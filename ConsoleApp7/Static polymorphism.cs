7274ad3c261f05c1718511d0f8827788177a4d2b
Shripad2607 <lokhandeshripad2@gmail.com>
2025-01-09 15:38:18 +0530

    addede

---
+++ ConsoleApp7/Static polymorphism.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class calculator
+    {
+        public int add(int a, int b)
+        {
+            return a + b;
+        }
+        public float add(float c,float d)
+        {
+            return c + d;
+        }
+    }
+    internal class staticpolymorphism
+    {
+        public static void Main()
+        {
+            calculator cal = new calculator();
+
+            int sum = cal.add(4, 2);
+            float sum1 = cal.add(5, 2);
+            Console.WriteLine("sum of first method" + sum);
+            Console.WriteLine("sum of second method" + sum1);
+        }
+    }
+}
