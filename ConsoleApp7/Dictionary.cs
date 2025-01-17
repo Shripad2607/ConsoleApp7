e39aac2f02aa2c0117794db04e0fb0aa85707347
Shripad2607 <lokhandeshripad2@gmail.com>
2025-01-17 19:42:15 +0530

    Stack and dictionary

---
+++ ConsoleApp7/Dictionary.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    internal class CollectionDictionary
+    {
+        static void Main()
+        {
+            Dictionary<int, string> dictionary = new Dictionary<int, string>
+            {
+                { 1,"Apple"},
+                { 2,"Banana"},
+                { 3,"Cherry"},
+                { 4,"Date"},
+                { 5,"Elderberry"},
+                { 6,"Fig"},
+                { 7,"Grape"},
+                { 8,"Honeybees"},
+                { 9,"Indian fig"},
+                { 10,"Jackfruit"}
+            };
+            //Display elements
+            Console.WriteLine("dictionary Elemets");
+            foreach(var items in dictionary)
+            {
+                Console.WriteLine($"Key:{items.Key},value:{items.Value}");
+            }
+            //Modify an elements
+            dictionary[2] = "Blueberry";
+            Console.WriteLine("\n Modified value for key 2 :" + dictionary[2]);
+
+            //Remove a specific elements
+            dictionary.Remove(4);
+            Console.WriteLine("\n After removing key 4:");
+            foreach(var items in dictionary)
+            {
+                Console.WriteLine($"Key:{items.Key},value:{items.Value}");
+            }
+            //filter elements using containkey
+            int keyToCheck = 5;
+            if(dictionary.ContainsKey(keyToCheck))
+            {
+                Console.WriteLine($"\nkey {keyToCheck} exist with value:{dictionary[keyToCheck]}");
+            }
+            else
+            {
+                Console.WriteLine($"\nkey {keyToCheck} does not exist");
+            }
+
+            //clear the dictionary
+            dictionary.Clear();
+            Console.WriteLine("\nDictionary cleared. Count :" + dictionary.Count);
+        }
+    }
+}
---
+++ ConsoleApp7/stack.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    internal class collection
+    {
+        static void Main()
+        {
+            Stack<object>stack = new Stack<object>();
+            stack.Push(1);
+            stack.Push("hello");
+            stack.Push(123456789);
+            stack.Push(false);
+            stack.Push(5.5);
+            Console.WriteLine("Total elements from stack");
+            foreach(var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("removed elements from stack");
+            object firstElement = stack.Pop();
+            object secondElement = stack.Pop();
+            object thirdElement = stack.Pop();
+            object fourthElement = stack.Pop();
+            object fifthElement = stack.Pop();
+
+            Console.WriteLine(firstElement);
+            Console.WriteLine(secondElement);
+            Console.WriteLine(thirdElement);
+            Console.WriteLine(fourthElement);
+            Console.WriteLine(fifthElement);
+
+            Console.WriteLine("Is stack empty :" + (stack.Count == 0));
+
+        }
+    }
+}
