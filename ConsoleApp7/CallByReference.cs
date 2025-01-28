e24366682eada0dca60eca0b3591bd2fd18f2e91
Shripad2607 <lokhandeshripad2@gmail.com>
2025-01-28 21:07:17 +0530

    added

---
+++ ConsoleApp7/CallByReference.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    internal class Program1
+    {
+        public void show(ref int val)
+        {
+            val*= val;
+            Console.WriteLine("Value inside show function " + val);
+        }
+        static void Main()
+        {
+            int val = 50;
+            Program1 Program = new Program1();
+            Console.WriteLine("Value before calling function " + val);
+            Program.show(ref val);
+            Console.WriteLine("values after calling function " + val);
+        }
+    }
+}
---
+++ ConsoleApp7/CallByValue.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        public void show(int val)
+        {
+            val *= val;
+            Console.WriteLine("Value inside show function" + val);
+        }
+        static void Main()
+        {
+            int val = 50;
+            Program Program = new Program();
+            Console.WriteLine("Value before calling function" + val);
+            Program.show(val);
+            Console.WriteLine("value after calling function" + val);
+        }
+    }
+}
